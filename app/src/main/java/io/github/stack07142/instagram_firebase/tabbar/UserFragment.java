d729cff47279c9757af4ada8ca9b8298f6fbff8d
stack07142 <stack.07142@gmail.com>
2017-09-10 20:35:53 +0900

    UserFragment-getFollow() 오류 수정: 버튼 상태 변경 안되는 현상

--- app/src/main/java/io/github/stack07142/instagram_firebase/tabbar/UserFragment.java
+++ app/src/main/java/io/github/stack07142/instagram_firebase/tabbar/UserFragment.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.RecyclerView;
@@ -317,11 +317,14 @@ public class UserFragment extends Fragment {
                         FollowDTO followDTO = dataSnapshot.getValue(FollowDTO.class);
                         try {
                             binding.accountTvFollowerCount.setText(String.valueOf(followDTO.followerCount));
-                            if (followDTO.followers.containsKey(uid)) {
+                            if (followDTO.followers.containsKey(currentUserUid)) {
+
+                                binding.accountBtnFollowSignout.setText(getString(R.string.follow_cancel));
                                 binding.accountBtnFollowSignout
                                         .getBackground()
-                                        .setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
+                                        .setColorFilter(ContextCompat.getColor(activity, R.color.colorLightGray), PorterDuff.Mode.MULTIPLY);
                             } else {
+                                binding.accountBtnFollowSignout.setText(getString(R.string.follow));
                                 binding.accountBtnFollowSignout
                                         .getBackground().setColorFilter(null);
                             }
